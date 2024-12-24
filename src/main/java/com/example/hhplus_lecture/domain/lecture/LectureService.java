fcd8c806b921b4d295ede2415ea8e7de2f8bac59
oeunkyoung <ou1112@naver.com>
2024-12-24 23:55:09 +0900

    ♻️Refactor: LectureService 인터페이스 생성, 기존 Service 클래스 구현 로직 LectureServiceImpl 클래스로 이동

--- src/main/java/com/example/hhplus_lecture/domain/lecture/LectureService.java
+++ src/main/java/com/example/hhplus_lecture/domain/lecture/LectureService.java
@@ -1,43 +1,15 @@
 package com.example.hhplus_lecture.domain.lecture;
 
-import com.example.hhplus_lecture.support.exception.BusinessException;
-import com.example.hhplus_lecture.support.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class LectureService {
+public interface LectureService {
 
-    private final LectureRepository lectureRepository;
-    private final LectureEnrollmentRepository lectureEnrollmentRepository;
+    /** 특강 신청 */
+    void enrollIn(long userId, long lectureId);
 
-    /** 특강 신청하기 */
-    @Transactional
-    public void enrollIn(long userId, long lectureId) {
-        LectureEnrollments lectureEnrollments = new LectureEnrollments(lectureEnrollmentRepository.findBy(userId, lectureId));
+    /** 신청 가능한 특강 목록 조회 (날짜) */
+    List<Lecture> findAvailableLectures(String startDate, String endDate);
 
-        Lecture lecture = lectureRepository.findById(lectureId);
-        // 특강 여석이 있는지
-        if (lecture.isFull()) {
-            throw new BusinessException(ErrorCode.LECTURE_FULL);
-        }
-        // 동일 사용자가 동일 특강에 신청한 내역이 있는지
-        if (lectureEnrollments.isEnrollmentExists(userId, lectureId)) {
-            throw new BusinessException(ErrorCode.DUPLICATE_REGISTRATION);
-        }
-
-        // 여석 차감
-        lecture.decreseRemainSeats();
-        lectureRepository.save(lecture);
-
-        lectureEnrollmentRepository.save(LectureEnrollment.of(userId, lectureId));
-    }
-
-    public List<Lecture> getLecturesByDate(String startDate, String endDate) {
-        return null;
-    }
+    /** 특정 사용자가 신청한 강의 목록 조회 */
+    List<LectureEnrollment> findLectureEnrollmentsByUserId(long userId);
 }
