d2162f6a1b0299ec7d076c30cc5d3c8a3123a7f0
oeunkyoung <ou1112@naver.com>
2024-12-25 16:58:28 +0900

    ✨Feat: Controller 구현

--- src/main/java/com/example/hhplus_lecture/interfaces/dto/lecture/LectureRequest.java
+++ src/main/java/com/example/hhplus_lecture/interfaces/dto/lecture/LectureRequest.java
@@ -1,6 +1,16 @@
 package com.example.hhplus_lecture.interfaces.dto.lecture;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LectureRequest {
+    @NotBlank
     private String startDate;
+
+    @NotBlank
     private String endDate;
 }
